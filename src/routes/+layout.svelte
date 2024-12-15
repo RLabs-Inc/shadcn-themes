2afadbd6379b11058a5f5db3e900eb277fa7dc8a
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-12-15 13:59:13 -0300

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-	import '../app.css';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+
+	import '../app.css';
+
 	let { children } = $props();
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 </script>
 
 <div class="grid h-screen grid-rows-[auto_1fr_auto]">
